1e243f1139791c49f81095c46e86af9900b7d3fd
sunkl <sunklmy@163.com>
2019-03-24 18:41:45 +0800

    初始化Spring Boot项目

---
+++ src/main/java/com/ly/imp4m/VideoResourcePlaybackSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ly.imp4m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoResourcePlaybackSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoResourcePlaybackSiteApplication.class, args);
+    }
+
+}
